dfd1505800b36124a7fbbcbb570c2fd9215d0bb3
Navaneeth Rao <decodestudios.help@gmail.com>
2023-11-13 12:58:53 +0530

    remove go vendor folder for now

--- go.mod
+++ go.mod
@@ -19,6 +19,8 @@ require (
 	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.5 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/k0kubun/go-ansi v0.0.0-20180517002512-3bf9e2903213 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
@@ -30,6 +32,7 @@ require (
 	github.com/moby/term v0.0.0-20221205130635-1aeaba878587 // indirect
 	github.com/sirupsen/logrus v1.9.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/vultr/govultr/v3 v3.4.0 // indirect
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/oauth2 v0.1.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
--- pkg/digitalocean/digitalocean.go
+++ pkg/digitalocean/digitalocean.go
@@ -2,21 +2,21 @@ package digitalocean
 
 import (
 	"context"
-	"fmt"
+
 	"github.com/digitalocean/godo"
-	"github.com/loft-sh/devpod/pkg/client"
 	"github.com/pkg/errors"
-	"time"
+	"github.com/vultr/govultr/v3"
+	"golang.org/x/oauth2"
 )
 
-func NewDigitalOcean(token string) *DigitalOcean {
-	return &DigitalOcean{
-		client: godo.NewFromToken(token),
+func NewVultr(token string) *Vultr {
+	return &Vultr{
+		client: govultr.NewClient(oauth2.NewClient(ctx, ts)),
 	}
 }
 
-type DigitalOcean struct {
-	client *godo.Client
+type Vultr struct {
+	client *vultr
 }
 
 func (d *DigitalOcean) Init(ctx context.Context) error {
@@ -28,151 +28,151 @@ func (d *DigitalOcean) Init(ctx context.Context) error {
 	return nil
 }
 
-func (d *DigitalOcean) Create(ctx context.Context, req *godo.DropletCreateRequest, diskSize int) error {
-	// create volume
-	volume, err := d.volumeByName(ctx, req.Name)
-	if err != nil {
-		return err
-	} else if volume == nil {
-		volume, _, err = d.client.Storage.CreateVolume(ctx, &godo.VolumeCreateRequest{
-			Region:          req.Region,
-			Name:            req.Name,
-			SizeGigaBytes:   int64(diskSize),
-			FilesystemType:  "ext4",
-			FilesystemLabel: "DevPod Data",
-			Tags:            []string{"devpod"},
-		})
-		if err != nil {
-			return errors.Wrap(err, "create volume")
-		}
-	}
+// func (d *DigitalOcean) Create(ctx context.Context, req *godo.DropletCreateRequest, diskSize int) error {
+// 	// create volume
+// 	volume, err := d.volumeByName(ctx, req.Name)
+// 	if err != nil {
+// 		return err
+// 	} else if volume == nil {
+// 		volume, _, err = d.client.Storage.CreateVolume(ctx, &godo.VolumeCreateRequest{
+// 			Region:          req.Region,
+// 			Name:            req.Name,
+// 			SizeGigaBytes:   int64(diskSize),
+// 			FilesystemType:  "ext4",
+// 			FilesystemLabel: "DevPod Data",
+// 			Tags:            []string{"devpod"},
+// 		})
+// 		if err != nil {
+// 			return errors.Wrap(err, "create volume")
+// 		}
+// 	}
 
-	// create droplet
-	req.Volumes = append(req.Volumes, godo.DropletCreateVolume{
-		ID: volume.ID,
-	})
-	_, _, err = d.client.Droplets.Create(ctx, req)
-	if err != nil {
-		return err
-	}
+// 	// create droplet
+// 	req.Volumes = append(req.Volumes, godo.DropletCreateVolume{
+// 		ID: volume.ID,
+// 	})
+// 	_, _, err = d.client.Droplets.Create(ctx, req)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
 
-func (d *DigitalOcean) volumeByName(ctx context.Context, name string) (*godo.Volume, error) {
-	volumes, _, err := d.client.Storage.ListVolumes(ctx, &godo.ListVolumeParams{Name: name})
-	if err != nil {
-		return nil, err
-	} else if len(volumes) > 1 {
-		return nil, fmt.Errorf("multiple volumes with name %s found", name)
-	} else if len(volumes) == 0 {
-		return nil, nil
-	}
+// func (d *DigitalOcean) volumeByName(ctx context.Context, name string) (*godo.Volume, error) {
+// 	volumes, _, err := d.client.Storage.ListVolumes(ctx, &godo.ListVolumeParams{Name: name})
+// 	if err != nil {
+// 		return nil, err
+// 	} else if len(volumes) > 1 {
+// 		return nil, fmt.Errorf("multiple volumes with name %s found", name)
+// 	} else if len(volumes) == 0 {
+// 		return nil, nil
+// 	}
 
-	return &volumes[0], nil
-}
+// 	return &volumes[0], nil
+// }
 
-func (d *DigitalOcean) Stop(ctx context.Context, name string) error {
-	droplet, err := d.GetByName(ctx, name)
-	if err != nil {
-		return err
-	} else if droplet == nil {
-		return nil
-	}
+// func (d *DigitalOcean) Stop(ctx context.Context, name string) error {
+// 	droplet, err := d.GetByName(ctx, name)
+// 	if err != nil {
+// 		return err
+// 	} else if droplet == nil {
+// 		return nil
+// 	}
 
-	_, err = d.client.Droplets.Delete(ctx, droplet.ID)
-	if err != nil {
-		return err
-	}
+// 	_, err = d.client.Droplets.Delete(ctx, droplet.ID)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
 
-func (d *DigitalOcean) Status(ctx context.Context, name string) (client.Status, error) {
-	// get droplet
-	droplet, err := d.GetByName(ctx, name)
-	if err != nil {
-		return client.StatusNotFound, err
-	} else if droplet == nil {
-		// check if volume exists
-		volume, err := d.volumeByName(ctx, name)
-		if err != nil {
-			return client.StatusNotFound, err
-		} else if volume != nil {
-			return client.StatusStopped, nil
-		}
+// func (d *DigitalOcean) Status(ctx context.Context, name string) (client.Status, error) {
+// 	// get droplet
+// 	droplet, err := d.GetByName(ctx, name)
+// 	if err != nil {
+// 		return client.StatusNotFound, err
+// 	} else if droplet == nil {
+// 		// check if volume exists
+// 		volume, err := d.volumeByName(ctx, name)
+// 		if err != nil {
+// 			return client.StatusNotFound, err
+// 		} else if volume != nil {
+// 			return client.StatusStopped, nil
+// 		}
 
-		return client.StatusNotFound, nil
-	}
+// 		return client.StatusNotFound, nil
+// 	}
 
-	// is busy?
-	if droplet.Status != "active" {
-		return client.StatusBusy, nil
-	}
+// 	// is busy?
+// 	if droplet.Status != "active" {
+// 		return client.StatusBusy, nil
+// 	}
 
-	return client.StatusRunning, nil
-}
+// 	return client.StatusRunning, nil
+// }
 
-func (d *DigitalOcean) GetByName(ctx context.Context, name string) (*godo.Droplet, error) {
-	droplets, _, err := d.client.Droplets.ListByName(ctx, name, &godo.ListOptions{})
-	if err != nil {
-		return nil, err
-	} else if len(droplets) > 1 {
-		return nil, fmt.Errorf("multiple droplets with name %s found", name)
-	} else if len(droplets) == 0 {
-		return nil, nil
-	}
+// func (d *DigitalOcean) GetByName(ctx context.Context, name string) (*godo.Droplet, error) {
+// 	droplets, _, err := d.client.Droplets.ListByName(ctx, name, &godo.ListOptions{})
+// 	if err != nil {
+// 		return nil, err
+// 	} else if len(droplets) > 1 {
+// 		return nil, fmt.Errorf("multiple droplets with name %s found", name)
+// 	} else if len(droplets) == 0 {
+// 		return nil, nil
+// 	}
 
-	return &droplets[0], nil
-}
+// 	return &droplets[0], nil
+// }
 
-func (d *DigitalOcean) Delete(ctx context.Context, name string) error {
-	// delete volume
-	volume, err := d.volumeByName(ctx, name)
-	if err != nil {
-		return err
-	} else if volume != nil {
-		// detach volume
-		for _, dropletID := range volume.DropletIDs {
-			_, _, err = d.client.StorageActions.DetachByDropletID(ctx, volume.ID, dropletID)
-			if err != nil {
-				return errors.Wrap(err, "detach volume")
-			}
-		}
+// func (d *DigitalOcean) Delete(ctx context.Context, name string) error {
+// 	// delete volume
+// 	volume, err := d.volumeByName(ctx, name)
+// 	if err != nil {
+// 		return err
+// 	} else if volume != nil {
+// 		// detach volume
+// 		for _, dropletID := range volume.DropletIDs {
+// 			_, _, err = d.client.StorageActions.DetachByDropletID(ctx, volume.ID, dropletID)
+// 			if err != nil {
+// 				return errors.Wrap(err, "detach volume")
+// 			}
+// 		}
 
-		// wait until volume is detached
-		for len(volume.DropletIDs) > 0 {
-			time.Sleep(time.Second)
+// 		// wait until volume is detached
+// 		for len(volume.DropletIDs) > 0 {
+// 			time.Sleep(time.Second)
 
-			// re-get volume
-			volume, err = d.volumeByName(ctx, name)
-			if err != nil {
-				return err
-			} else if volume == nil {
-				break
-			}
-		}
+// 			// re-get volume
+// 			volume, err = d.volumeByName(ctx, name)
+// 			if err != nil {
+// 				return err
+// 			} else if volume == nil {
+// 				break
+// 			}
+// 		}
 
-		// delete volume
-		if volume != nil {
-			_, err = d.client.Storage.DeleteVolume(ctx, volume.ID)
-			if err != nil {
-				return errors.Wrap(err, "delete volume")
-			}
-		}
-	}
+// 		// delete volume
+// 		if volume != nil {
+// 			_, err = d.client.Storage.DeleteVolume(ctx, volume.ID)
+// 			if err != nil {
+// 				return errors.Wrap(err, "delete volume")
+// 			}
+// 		}
+// 	}
 
-	droplet, err := d.GetByName(ctx, name)
-	if err != nil {
-		return err
-	} else if droplet == nil {
-		return nil
-	}
+// 	droplet, err := d.GetByName(ctx, name)
+// 	if err != nil {
+// 		return err
+// 	} else if droplet == nil {
+// 		return nil
+// 	}
 
-	_, err = d.client.Droplets.Delete(ctx, droplet.ID)
-	if err != nil {
-		return err
-	}
+// 	_, err = d.client.Droplets.Delete(ctx, droplet.ID)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
